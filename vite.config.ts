89306dc784c3605a6cfe3f5e12255a59ea550a7c
Ryco Buckton <rycobuckton@gmail.com>
2026-07-12 16:18:09 -0300

    Att12

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-const repoName = 'Acorde-Visual-Midi-navegador';
+const repoName = 'Acorde-Visual-Midi-On-Line';
 
 export default defineConfig(() => {
   return {
